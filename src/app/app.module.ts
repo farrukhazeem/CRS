2487dff0ee026e824e928e63a0683f9722dc15de
farrukhazeem <farrukh90@hotmail.com>
2017-10-17 16:40:33 +0500

    2nd commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,15 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FormxComponent } from './formx/formx.component';
+import { TextComponent } from './text/text.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FormxComponent
+    FormxComponent,
+    TextComponent,
+    
   ],
   imports: [
     BrowserModule,
